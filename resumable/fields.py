b342efd414a8825304a7ebdde98a5bef6b31c656
Raphaël Stefanini <raphael@rphlo.com>
2022-01-28 09:10:43 +0200

    DJango4

--- resumable/fields.py
+++ resumable/fields.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.forms.fields import FileField
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .widgets import ResumableFileInput
 
